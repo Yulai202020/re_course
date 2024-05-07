7ba2d41a67ddc51b71c296109fa843aff245f43f
Yulai202020 <ngmyulai@gmail.com>
2024-05-07 20:00:55 +0400

    first commit

--- 4.py
+++ 4.py
@@ -1,3 +1,14 @@
 import re
 
-pattern = re.compile(r"")
+text = ""
+
+with open("test.txt") as f:
+    text = f.read()
+
+# find all time codes with titles
+
+pattern = re.compile(r".*(\b\d{2}:\d{2}:\d{2}\b).*")
+a = re.finditer(pattern, text)
+
+for i in a:
+    print("line with link:", i.group(0))
